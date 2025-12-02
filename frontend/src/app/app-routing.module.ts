f211a9b3e66d7a46458f5151c42f2b7d6924fcfb
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-01 23:16:04 -0300

    feat: adiciona rotas para os componentes principais do aplicativo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { CertificatesComponent } from './pages/certifications/certificates.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'home',
+    component: MainComponent,
+    children: [
+      {
+        path: 'projects',
+        component: ProjectsComponent,
+      },
+      {
+        path: 'technologies',
+        component: TechnologiesComponent,
+      },
+      {
+        path: 'certificates',
+        component: CertificatesComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
