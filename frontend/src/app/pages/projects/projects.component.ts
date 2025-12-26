01acd91552e9333912828153e275bc722b9f7ada
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-26 09:09:20 -0300

    feat: implementa lista de projetos com links e tecnologias

--- frontend/src/app/pages/projects/projects.component.ts
+++ frontend/src/app/pages/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from '../../shared/interfaces/projects.interface'
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects: Project[] = [
+    {
+      title: 'Projeto Financeiro',
+      description: 'Sistema para controle financeiro de pequenos negócios.',
+      image: 'https://placehold.co/250x250',
+      alt: 'Projeto Financeiro',
+      techs: ['HTML', 'CSS', 'JavaScript', 'LocalStorage'],
+      demoLink: '#',
+      codeLink: '#',
+    },
+    {
+      title: 'App de Pizzaria',
+      description: 'Gerenciamento de pedidos, clientes e pagamentos.',
+      image: 'https://placehold.co/250x250',
+      alt: 'App de Pizzaria',
+      techs: ['HTML', 'CSS', 'JavaScript', 'API'],
+      demoLink: '#',
+      codeLink: '#',
+    },
+    {
+      title: 'Landing Page',
+      description: 'Landing page moderna, rápida e responsiva.',
+      image: 'https://placehold.co/250x250',
+      alt: 'Landing Page',
+      techs: ['HTML', 'CSS', 'Responsivo'],
+      demoLink: '#',
+      codeLink: '#',
+    },
+  ]
 }
---
+++ frontend/src/app/shared/interfaces/projects.interface.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  title: string;
+  description: string;
+  image: string;
+  alt: string;
+  techs: string[];
+  demoLink: string;
+  codeLink: string;
+}
